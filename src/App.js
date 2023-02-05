06fd891005a0b6d7d104f04086c407025baa469e
seula <seulaayoon@gmail.com>
2023-02-05 16:47:53 +0900

    ref practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import EventPractice from './EventPractice';
+import ScrollBox from './ScrollBox';
 import './App.css';
+import { Component } from 'react';
 
-const App = () => {
-  return <EventPractice />;
+class App extends Component{
+render () {
+  return (
+    <div>
+      <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+      <button onClick={() => this.ScrollBox.scrollToBottom()}>
+        맨 밑으로
+      </button>
+    </div>);
 };
+}
 
 export default App;
