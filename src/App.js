b5be83fa61bb58716f167199dbe7366cc752c174
seula <seulaayoon@gmail.com>
2023-02-08 13:07:48 +0900

    Iteration Practice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import ScrollBox from './ScrollBox';
+
 import './App.css';
 import { Component } from 'react';
+import IterationSample from './IterationSample';
 
 class App extends Component{
 render () {
   return (
-    <div>
-      <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
-      <button onClick={() => this.ScrollBox.scrollToBottom()}>
-        맨 밑으로
-      </button>
-    </div>);
+    <IterationSample/>
+    )
 };
 }
 
